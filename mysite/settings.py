f4d2b1d511dcba863b06ea3fbf7cb18a5253c50f
OlgaRiabkova <oliabric@gmail.com>
2023-04-08 12:45:18 +0500

    +

--- mysite/settings.py
+++ mysite/settings.py
@@ -78,7 +78,7 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
-    },
+    }
     # 'default': {
     #     'ENGINE': 'django.db.backends.mysql',
     #     'HOST': '/var/run/mysqld/mysqld.sock',
